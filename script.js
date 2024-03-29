59ed584140ca55a48ded20ba8829c4177f5277d6
Marouane Bouzzit <71093211+mrwn14@users.noreply.github.com>
2024-03-29 15:45:25 -0400

    works for gmail emails too

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === "readText") {
+    readTextFromClass("XbIp4"); //outlook
+    readTextFromClass("a3s"); //gmail
+  }
+});
+
 function readTextFromClass(className) {
   let elements = document.querySelectorAll("." + className);
   let allText = "";
   elements.forEach(function (element) {
     allText += element.innerText.trim() + "\n";
   });
+  // Add api to send to model
   console.log(allText);
 }
-
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === "readText") {
-    readTextFromClass("XbIp4");
-  }
-});
