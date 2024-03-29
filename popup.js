5cac2a4dd166ab18fd3d555d866aeb9093c0eedf
Marouane Bouzzit <71093211+mrwn14@users.noreply.github.com>
2024-03-29 18:24:48 -0400

    added wait

--- popup.js
+++ popup.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
   readButton.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { action: "readText" });
-      console.log(tabs[0]);
     });
   });
 });
--- script.js
+++ script.js
@@ -6,6 +6,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 });
 
 function readTextFromClass(className) {
+  var loadingDiv = document.getElementById("loading");
   let elements = document.querySelectorAll("." + className);
   let allText = "";
   elements.forEach(function (element) {
@@ -13,4 +14,25 @@ function readTextFromClass(className) {
   });
   // Add api to send to model
   console.log(allText);
+  if (loadingDiv) {
+    loadingDiv.style.display = "block";
+    fakeAsyncFunction()
+      .then(function (percentage) {
+        console.log("Percentage:", percentage);
+        loadingDiv.style.display = "none";
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+        loadingDiv.style.display = "none";
+      });
+  }
+}
+
+function fakeAsyncFunction() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      var percentage = 50;
+      resolve(percentage);
+    }, 4000);
+  });
 }
